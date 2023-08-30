25a7a6b361d109f6d1bb9eb99301ac3f964d9dad
Sid-d-harth <34siddharth007@gmail.com>
2023-08-30 22:42:24 +0530

    30-08-23

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { loadStripe } from '@stripe/stripe-js';
+import { asyncScheduler } from 'rxjs';
 import { Cart, CartItem } from 'src/app/models/cart.model';
 import { CartService } from 'src/app/services/cart.service';
 
@@ -24,7 +27,7 @@ export class CartComponent implements OnInit {
     'action',
   ];
 
-  constructor(private cartService: CartService) { }
+  constructor(private cartService: CartService, private http: HttpClient) { }
 
   ngOnInit(): void{
     this.cartService.cart.subscribe((_cart: Cart) => {
@@ -51,5 +54,16 @@ export class CartComponent implements OnInit {
 
   onRemoveQuantity(item: CartItem):void {
     this.cartService.removeQuantity(item);
+  }
+
+  onCheckout(): void {
+    this.http.post('http://localhost:4242/checkout', {
+      items: this.cart.items
+    }).subscribe(async(res: any) => {
+      let stripe = await loadStripe('pk_test_51NkqUnSHhm2eYUzjjamXMNLZhj90JCAovdIDuIZ6LrmiEchZzHZN7eQGS1BMj3eKrHd4eKq3ck5CKEx2ODUgjb1B00oRFfVzoN');
+      stripe?.redirectToCheckout({
+        sessionId: res.id
+      })
+    });
   }
 }
--- src/app/pages/home/components/filters/filters.component.ts
+++ src/app/pages/home/components/filters/filters.component.ts
@@ -1,15 +1,32 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
+import { Subscription, subscribeOn } from 'rxjs';
+import { StoreService } from 'src/app/services/store.service';
 
 @Component({
   selector: 'app-filters',
   templateUrl: 'filters.components.html',
 })
-export class FiltersComponent {
-  @Output() showCategory = new EventEmitter <string> ();
+export class FiltersComponent implements OnInit, OnDestroy {
+  @Output() showCategory = new EventEmitter<string>();
+  categoriesSubscription: Subscription | undefined;
+  categories: Array<string> | undefined;
 
-  categories = ['shoes', 'sports'];
+  constructor(private storeService: StoreService) { }
+
+  ngOnInit(): void {
+    this.categoriesSubscription = this.storeService.getAllCategories()
+    .subscribe((response) => {
+      this.categories = response;
+    });
+  }
 
   onShowCategory(category: string): void{
     this.showCategory.emit(category);
+  }
+
+  ngOnDestroy(): void {
+    if (this.categoriesSubscription) {
+      this.categoriesSubscription.unsubscribe();
+    }
   }
 }
